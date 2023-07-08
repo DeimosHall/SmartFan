3763bd91974fef4636fe57969bb2c9cfa8b5914b
deimos <deimoshall@gmail.com>
2023-07-08 08:58:32 -0600

    refactor: add ezoutput

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 BOARD_TAG = arduino:mbed_rp2040:pico
 # MONITOR_PORT = /dev/ttyACM0
-MONITOR_PORT = /dev/cu.usbmodem114401
+MONITOR_PORT = /dev/cu.usbmodem11401
 USER_NAME := $(shell whoami)
 # ARDUINO_CLI_PATH := /home/$(USER_NAME)/arduino-cli/arduino-cli
 ARDUINO_CLI_PATH := arduino-cli
--- SmartFan.ino
+++ SmartFan.ino
@@ -1,36 +1,31 @@
 #include <OneWire.h>
 #include <DallasTemperature.h>
+#include <ezOutput.h>
 
 #define FAN_PIN 28
 #define TEMPERATURE_PIN 27
 
-bool fanState = false;
-
 OneWire oneWire(TEMPERATURE_PIN);
 DallasTemperature sensor(&oneWire);
+ezOutput fan(FAN_PIN);
 
 void setup() {
   Serial.begin(9600);
-
   sensor.begin();  // Start the DS18B20 sensor
+  fan.low();
 }
 
 void loop() {
-  Serial.println("Temperature: " + String(getTemperature(), 2) + "°C");
-  delay(1000);
+  static unsigned long elapsedTime = millis();
+
+  if (millis() - elapsedTime > 1000) {
+    elapsedTime = millis();
+    Serial.println("Temperature: " + String(getTemperature(), 2) + "°C");
+    Serial.println("Fan: " + String(fan.getState() ? "ON" : "OFF"));
+  }  
 }
 
 float getTemperature() {
   sensor.requestTemperatures();
   return sensor.getTempCByIndex(0);
-}
-
-void fanOn() {
-  digitalWrite(FAN_PIN, HIGH);
-  fanState = true;
-}
-
-void fanOff() {
-  digitalWrite(FAN_PIN, LOW);
-  fanState = false;
 }
